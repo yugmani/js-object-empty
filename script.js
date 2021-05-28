73b3433f61443f447a4df42da5cb1d9b2d8f5e8b
yugmani <yugmani@yahoo.com>
2021-05-27 22:01:34 -0700

    ES+5 way using Object.keys(obj) method

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Pre - ES5 Way
+//1. Pre - ES5 Way
 function isEmptyObject(obj) {
   for (let property in obj) {
     if (obj.hasOwnProperty(property)) {
@@ -8,6 +8,18 @@ function isEmptyObject(obj) {
   return true;
 }
 
-console.log(isEmptyObject({})); //Expected true;
-console.log(isEmptyObject({ foo: '1' })); //Expected false;
+// console.log(isEmptyObject({})); //Expected true;
+// console.log(isEmptyObject({ foo: '1' })); //Expected false;
 
+//2. The ES5+ Way
+// The Object.keys() method returns an array of enumerable property names of a given object.
+
+function hasPropertyObject(obj) {
+  return Object.keys(obj).length === 0;
+}
+
+console.log(hasPropertyObject({})); //Expected true;
+
+let bar = { foo: '1' };
+console.log(Object.keys(bar).length); //Expedted 1;
+console.log(hasPropertyObject(bar)); //Expected false;
